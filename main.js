173a2319b40d61e4ddbac3abd5327841868f5ba1
diasmark77 <diasmarkabayev@gmail.com>
2026-02-28 19:23:36 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,19 @@
-<script type="module">
-  // Firebase негізгі модульдер
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
-  import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
+// main.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
-  // Firebase config (сенің кілттерің)
-  const firebaseConfig = {
-    apiKey: "AIzaSyCGwoBeiG0blGR0_GTQ74FFo0gew95-dBw",
-    authDomain: "adebietordasy.firebaseapp.com",
-    projectId: "adebietordasy",
-    storageBucket: "adebietordasy.firebasestorage.app",
-    messagingSenderId: "933110984004",
-    appId: "1:933110984004:web:a9a9dba11325950ed1fb11"
-  };
+// 🔹 Firebase конфигурациясы
+const firebaseConfig = {
+  apiKey: "AIzaSyC_aLF4dK_m0jvkbSzhTWo3rsP7yx5uoXw",
+  authDomain: "adebietordasy.firebaseapp.com",
+  projectId: "adebiet-ordasy",
+  storageBucket: "adebietordasy.firebasestorage.app",
+  messagingSenderId: "331787955330",
+  appId: "1:331787955330:web:a77853b5bffdb44bed989b",
+};
 
-  // Инициализация
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const db = getFirestore(app);
-
-  // Басқа беттерде қолдану үшін экспорт
-  export { app, auth, db };
-</script>
+// 🔹 Firebase инициализациясы
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
