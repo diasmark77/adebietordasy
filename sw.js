6fa165daf6bde46baeef8e303933df5ffa376a3d
diasmark77 <diasmarkabayev@gmail.com>
2026-05-13 10:28:37 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,26 @@
-const CACHE = 'adebiet-v1';
+const CACHE = 'adebiet-v2';
 const FILES = [
   '/adebietordasy/',
-  '/adebietordasy/index.html'
+  '/adebietordasy/index.html',
+  '/adebietordasy/manifest.json',
+  '/adebietordasy/images/icon-192.png',
+  '/adebietordasy/images/icon-512.png'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+    .then(() => self.skipWaiting()) // ← дереу активтеу
+  );
+});
+
+self.addEventListener('activate', e => {
+  // Ескі кэшті тазалау
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
 
 self.addEventListener('fetch', e => {
