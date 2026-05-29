ccb295c56243769f31df477a73212e268f1f0aa0
diasmark77 <diasmarkabayev@gmail.com>
2026-05-29 15:17:25 +0500

    Upgrade service worker cache version to v3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'adebiet-v2';
+const CACHE = 'adebiet-v3';
 const FILES = [
   '/adebietordasy/',
   '/adebietordasy/index.html',
@@ -10,21 +10,27 @@ const FILES = [
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(FILES))
-    .then(() => self.skipWaiting()) // ← дереу активтеу
+    .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', e => {
-  // Ескі кэшті тазалау
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim()) // ← БАР беттерді жаңарту
   );
 });
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    // Алдымен желіден алады, қатеде кэштен береді
+    fetch(e.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
